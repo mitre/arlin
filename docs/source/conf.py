4b52a96a14fca556b2d2f1328a8f6380c2af3468
Alex Tapley <atapley@mitre.org>
2023-07-25 15:13:29 +0000

    docs

---
+++ docs/source/conf.py
@@ -0,0 +1,74 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+
+project = 'ARLIN - Adversarial Reinforcement Learning Model Interrogation'
+copyright = '2023, MITRE'
+author = 'Alex Tapley'
+
+version = "0.1"
+
+# The full version, including alpha/beta/rc tags
+if os.environ.get("CI_COMMIT_TAG"):
+    release = os.environ["CI_COMMIT_TAG"]
+else:
+    release = "latest"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.coverage",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "autoapi.extension",
+    "myst_parser",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "networkx": ("https://networkx.org/documentation/stable/", None),
+    "stable-baselines3": ("https://stable-baselines3.readthedocs.io/en/master/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None)
+}
+
+# Autodoc settings
+autodoc_typehints = "description"
+
+# Autoapi settings
+autoapi_options = ["members", "undoc-members", "show-inheritance", "show-module-summary"]
+autoapi_python_class_content = "both"
+autoapi_type = "python"
+autoapi_dirs = ["../../arlin/"]
+
+templates_path = ['_templates']
+exclude_patterns = ["**/_tests"]
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+html_logo = "../images/arlin_logo.png"
+html_theme_options = {
+    "logo_only": False,
+    "display_version": True,
+}
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,32 @@
+.. ARLIN documentation master file, created by
+   sphinx-quickstart on Tue Jul 25 13:17:13 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ARLIN
+Adversarial Reinforcement Learning Model Interrogation
+=================================
+
+**ARLIN** is an open-source Python library for researchers within both the explainable
+AI (XAI) and adversarial AI (AdvAI) fields. It utilizes 
+`matplotlib <https://matplotlib.org/stable/>`_ and 
+`networkx <https://networkx.org/documentation/stable/>`_ to create meaningful metric 
+visualizations for researchers to identify vulnerabilities within trained reinforcement
+learning (RL) models and offers a *simple* and *intuitive* API along with a *modular* 
+code architecture.
+
+.. note::
+   This project is under active development through 2023 under the Decision Science ECRP.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
